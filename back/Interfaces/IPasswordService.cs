e0541562319ed7bd32adfe597939f88e778a2477
Snt <sant85082@gmail.com>
2026-06-24 15:50:23 -0500

    feat: implement password hashing and verifying logic
    implement the interface and class with the methods to hash the password and check if thepassword is correct with another password hash

---
+++ back/Interfaces/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace back.Interfaces
+{
+    public interface IPasswordService
+    {
+        public string Hash(string password);
+        public bool Verify(string password, string hashPassword);
+    }
+}
---
+++ back/Services/PasswordService.cs
@@ -0,0 +1,19 @@
+﻿using BCrypt.Net;
+using back.Interfaces;
+namespace back.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        private const int WorkFactor = 12;
+        public string Hash(string password)
+        {
+            string hashPassword = BCrypt.Net.BCrypt.HashPassword(password, workFactor: WorkFactor); 
+            return hashPassword;
+        }
+        public bool Verify(string password, string hashPassword)
+        {
+            bool isPasswordCorrect = BCrypt.Net.BCrypt.Verify(password, hashPassword);
+            return isPasswordCorrect;
+        }
+    }
+}
