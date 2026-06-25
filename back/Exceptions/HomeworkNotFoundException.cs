2e6c69c51abe7bc3c313ca4effc94b86046f9f12
Snt <sant85082@gmail.com>
2026-06-24 19:43:35 -0500

    feat: implement HomeworkNotFoundException custom exception
    implement the logic fot the exception in cases when the homework is not found

---
+++ back/Exceptions/HomeworkNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace back.Exceptions
+{
+    public class HomeworkNotFoundException : Exception
+    {
+        public HomeworkNotFoundException(string description) : base(description)
+        {
+
+        }
+    }
+}
