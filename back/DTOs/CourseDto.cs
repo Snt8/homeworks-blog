65a95419d0f0bbf7839a7c7efd2d3c2730309d3d
Snt <sant85082@gmail.com>
2026-06-25 16:59:10 -0500

    feat: implement course DTO and update the Model
    implement the DTO with the input data from the client and update the model with course name, owner and owner id

---
+++ back/DTOs/CourseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace back.DTOs
+{
+    public class CourseDto
+    {
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
--- back/Models/Course.cs
+++ back/Models/Course.cs
@@ -4,10 +4,14 @@ namespace back.Models
     {
         // Course unique identifier
         public int Id { get; set; }
+        public string Name { get; set; }
+        //Course creator 
+        public User Owner { get; set; }
+        public int OwnerId { get; set; }
         // Students registered in the course
-        public List<User> Students { get; set; }
+        public List<User> Students { get; set; } = new List<User>();
         // Homeworks assigned for the course
-        public List<Homework> Homeworks { get; set; }
+        public List<Homework> Homeworks { get; set; } = new List<Homework>();
 
     }
 }
