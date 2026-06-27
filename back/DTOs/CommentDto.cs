8af1945ba2d49d518cdfa1ea3ca82ae041a857b4
Snt <sant85082@gmail.com>
2026-06-27 18:35:46 -0500

    feat: implement comment DTO

---
+++ back/DTOs/CommentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace back.DTOs
+{
+    public class CommentDto
+    {
+        public string Content { get; set; }
+        public int UserId { get; set; }
+        public int HomeworkId {  get; set; }
+
+    }
+}
