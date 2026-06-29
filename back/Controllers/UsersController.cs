392bb9057bae9c058cac2e41f15bd18f8ea7ee8d
Snt <sant85082@gmail.com>
2026-06-29 11:11:27 -0500

    feat: implement endpoints for new user service methods

--- back/Controllers/UsersController.cs
+++ back/Controllers/UsersController.cs
@@ -50,5 +50,58 @@ namespace back.Controllers
                 return Unauthorized(new { message = ex.Message });
             }
         }
+
+        //PUT /api/users/{userId}/enroll/{courseId}
+        [HttpPut("{userId}/enroll/{courseId}")]
+        public async Task<ActionResult<User>> EnrollInCourse(int userId, int courseId)
+        {
+            try
+            {
+                var user = await _userService.RegisterCourseUser(userId, courseId);
+                return Ok(user);
+            }
+            catch (UserNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (CourseNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+            catch (UserIsAlreadyInACourse ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        //PUT /api/users/{userId}
+        [HttpPut("{userId}")]
+        public async Task<ActionResult<User>> UpdateUser(int userId, [FromBody] UserUpdateDto request)
+        {
+            try
+            {
+                var user = await _userService.UpdateUser(request, userId);
+                return Ok(user);
+            }
+            catch (UserNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+        //DELETE /api/users/{userId}
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult<DeletedUserDto>> DeleteUser(int userId)
+        {
+            try
+            {
+                var deletedUser = await _userService.DeleteUser(userId);
+                return Ok(deletedUser);
+            }
+            catch (UserNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
     }
 }
